fab54330aa00dbada75e31db699750466357e133
ChrisKnowsDev <chrisharley81@gmail.com>
2020-12-10 17:07:54 -0500

    created navbar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,61 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import styled from 'styled-components'
 
 import { rhythm, scale } from '../utils/typography'
+import Navbar from './navbar'
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    const blogPath = `${__PATH_PREFIX__}/blog/`
-    let header
+    const { children } = this.props
 
-    if (location.pathname === rootPath || location.pathname === blogPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`
-            }}
-            to={location.pathname === blogPath ? `/blog/` : `/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
       <Wrapper>
-        <div
+        <Navbar />
+        <main
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
@@ -63,9 +19,8 @@ class Layout extends React.Component {
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
           }}
         >
-          <header>{header}</header>
-          <main>{children}</main>
-        </div>
+          {children}
+        </main>
         <Footer>© {new Date().getFullYear()}, wutNow</Footer>
       </Wrapper>
     )
